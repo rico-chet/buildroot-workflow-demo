217e0e9a190652c5f7ac9229bc0bbd2d06424250
rico-chet <rico-chet@users.noreply.github.com>
2022-12-13 09:52:18 +0000

    Switch to a working C package

--- my-package/my-package.mk
+++ my-package/my-package.mk
@@ -2,8 +2,14 @@ MY_PACKAGE_VERSION = 1.0
 MY_PACKAGE_SITE_METHOD = local
 MY_PACKAGE_SITE = $(TOPDIR)/package/my-package/src
 
-define MY_PACKAGE_BUILD_CMDS
-	true
+define HOST_MY_PACKAGE_BUILD_CMDS
+	cd $(@D) \
+	&& $(HOSTCC) $(@D)/my-code.c
+endef
+
+define HOST_MY_PACKAGE_INSTALL_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/a.out $(HOST_DIR)/bin/my-package
+	$(HOST_DIR)/bin/my-package
 endef
 
 $(eval $(host-generic-package))
